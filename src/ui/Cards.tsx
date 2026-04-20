de7acdc76f0fde75161b72c6f9747105af5cea6b
Angad <angad04755@gmail.com>
2026-04-21 02:01:27 +0530

    changes

--- src/ui/Cards.tsx
+++ src/ui/Cards.tsx
@@ -14,7 +14,7 @@ function Cards({heading, desc, text}: CardProps) {
             <div className="flex flex-col gap-5">
                 <h3 className="text-white text-xl">{heading}</h3>
                 <p className="text-left text-gray-700">{desc}</p>
-                <button className="bg-gray-700 text-white py-3 px-3 rounded-xl hover:active-95 transition-all duration-200">{text}</button>
+                <button className="bg-gray-700 text-white py-3 px-3 rounded-xl cursor-pointer hover:scale-95 transition-all duration-200">{text}</button>
             </div>
         </div>
     )
